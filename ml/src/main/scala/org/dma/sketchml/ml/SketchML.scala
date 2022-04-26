1172b2a152c760b275270f43a0eccfba290038e8
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-04-26 07:24:19 +0000

    Enable strict mode

--- ml/src/main/scala/org/dma/sketchml/ml/SketchML.scala
+++ ml/src/main/scala/org/dma/sketchml/ml/SketchML.scala
@@ -2,4 +2,9 @@ package org.dma.sketchml.ml
 
 import org.apache.spark.{SparkConf, SparkContext}
 import org.dma.sketchml.ml.algorithm._
-import org.dma.sketchml.ml.common.Constan
+import org.dma.sketchml.ml.common.Constants
+import org.dma.sketchml.ml.conf.MLConf
+
+object SketchML {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().
