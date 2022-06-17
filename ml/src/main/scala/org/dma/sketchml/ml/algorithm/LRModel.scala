03da3ce65bd9bdd4f775b5fb5a854fa076d3d1ca
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-06-17 13:18:18 +0000

    appease rubocop

--- ml/src/main/scala/org/dma/sketchml/ml/algorithm/LRModel.scala
+++ ml/src/main/scala/org/dma/sketchml/ml/algorithm/LRModel.scala
@@ -2,4 +2,9 @@ package org.dma.sketchml.ml.algorithm
 
 import org.apache.spark.ml.linalg.DenseVector
 import org.dma.sketchml.ml.algorithm.GeneralizedLinearModel.Model._
-import org.d
+import org.dma.sketchml.ml.common.Constants
+import org.dma.sketchml.ml.conf.MLConf
+import org.dma.sketchml.ml.objective.{Adam, L2LogLoss}
+import org.slf4j.{Logger, LoggerFactory}
+
+obje
