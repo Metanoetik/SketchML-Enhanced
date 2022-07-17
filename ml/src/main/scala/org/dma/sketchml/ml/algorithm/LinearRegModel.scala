d2872788cb2754e0c160b2e98636bbb3b966cd63
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-07-17 23:00:40 +0000

    minor changes to README

--- ml/src/main/scala/org/dma/sketchml/ml/algorithm/LinearRegModel.scala
+++ ml/src/main/scala/org/dma/sketchml/ml/algorithm/LinearRegModel.scala
@@ -1,1 +1,5 @@
-package or
+package org.dma.sketchml.ml.algorithm
+
+import org.apache.spark.ml.linalg.DenseVector
+import org.dma.sketchml.ml.algorithm.GeneralizedLinearModel.Model._
+import org.dma.sketchml.ml.common.Cons
