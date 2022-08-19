90c2cec2c1d983c9d3c6c26078b1b1b5c72c4963
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-08-19 22:55:33 +0000

    Initializing repository

--- ml/src/main/scala/org/dma/sketchml/ml/algorithm/SVMModel.scala
+++ ml/src/main/scala/org/dma/sketchml/ml/algorithm/SVMModel.scala
@@ -1,4 +1,7 @@
 package org.dma.sketchml.ml.algorithm
 
 import org.apache.spark.ml.linalg.DenseVector
-import org.dma.sketchml.ml.algorithm.Generaliz
+import org.dma.sketchml.ml.algorithm.GeneralizedLinearModel.Model._
+import org.dma.sketchml.ml.common.Constants
+import org.dma.sketchml.ml.conf.MLConf
+import org.dma.sketchml.ml.objective.{Adam, L2HingeL
