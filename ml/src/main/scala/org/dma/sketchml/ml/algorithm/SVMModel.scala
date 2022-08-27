66b8ae7bcd8dc835ef8696226ad3f05c250d7816
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-08-27 16:15:02 +0000

    BUG

--- ml/src/main/scala/org/dma/sketchml/ml/algorithm/SVMModel.scala
+++ ml/src/main/scala/org/dma/sketchml/ml/algorithm/SVMModel.scala
@@ -4,4 +4,10 @@ import org.apache.spark.ml.linalg.DenseVector
 import org.dma.sketchml.ml.algorithm.GeneralizedLinearModel.Model._
 import org.dma.sketchml.ml.common.Constants
 import org.dma.sketchml.ml.conf.MLConf
-import org.dma.sketchml.ml.objective.{Adam, L2HingeL
+import org.dma.sketchml.ml.objective.{Adam, L2HingeLoss}
+import org.slf4j.{Logger, LoggerFactory}
+
+object SVMModel {
+  private val logger: Logger = LoggerFactory.getLogger(SVMModel.getClass)
+
+  def apply(conf: MLConf): SVMModel = new SVMMo
