be9d62cdb69393baa90728ab49940fbcdcedc337
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-08-28 10:07:51 +0000

    0.3.12

--- ml/src/main/scala/org/dma/sketchml/ml/common/Constants.scala
+++ ml/src/main/scala/org/dma/sketchml/ml/common/Constants.scala
@@ -2,4 +2,8 @@ package org.dma.sketchml.ml.common
 
 object Constants {
   val ML_LOGISTIC_REGRESSION: String = "LogisticRegression"
-  val ML_SUPPORT_VECTOR_MACHINE: String = "SupportVectorMachi
+  val ML_SUPPORT_VECTOR_MACHINE: String = "SupportVectorMachine"
+  val ML_LINEAR_REGRESSION: String = "LinearRegression"
+  val FORMAT_LIBSVM: String = "libsvm"
+  val FORMAT_CSV: String = "csv"
+  val FORMAT_DUMMY:
