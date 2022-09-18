6d25b7d1a505cbed6c3f5dadd99862f5220d579c
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-09-18 17:39:19 +0000

    Refactor files

--- ml/src/main/scala/org/dma/sketchml/ml/objective/Loss.scala
+++ ml/src/main/scala/org/dma/sketchml/ml/objective/Loss.scala
@@ -3,4 +3,11 @@ package org.dma.sketchml.ml.objective
 import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.dma.sketchml.ml.util.Maths
 
-tr
+trait Loss extends Serializable {
+  def loss(pre: Double, y: Double): Double
+
+  def grad(pre: Double, y: Double): Double
+
+  def predict(w: Vector, x: Vector): Double
+
+  def 
