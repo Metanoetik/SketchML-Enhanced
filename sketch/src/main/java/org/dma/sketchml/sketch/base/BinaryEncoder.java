34bf4d6533080adbd916b117e44cf1599f197e10
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-12-04 08:19:56 +0000

    merged from funtoo-staging

--- sketch/src/main/java/org/dma/sketchml/sketch/base/BinaryEncoder.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/base/BinaryEncoder.java
@@ -3,4 +3,9 @@ package org.dma.sketchml.sketch.base;
 import java.io.Serializable;
 import java.util.stream.IntStream;
 
-public interface BinaryEnco
+public interface BinaryEncoder extends Serializable {
+    void encode(int[] values);
+
+    int[] decode();
+
+}
---
+++ sketch/src/main/java/org/dma/sketchml/sketch/base/Int2IntHash.java
@@ -0,0 +1,5 @@
+package org.dma.sketchml.sketch.base;
+
+import java.io.Serializable;
+
+p
