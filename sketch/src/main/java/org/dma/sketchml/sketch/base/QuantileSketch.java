eaa8f3549cb08a800c344976af98068dc9c270d9
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-12-04 11:50:03 +0000

    Add a Makefile

--- sketch/src/main/java/org/dma/sketchml/sketch/base/QuantileSketch.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/base/QuantileSketch.java
@@ -4,4 +4,6 @@ package org.dma.sketchml.sketch.base;
 import java.io.Serializable;
 
 public abstract class QuantileSketch implements Serializable {
-    pr
+    protected long n; // total number of data items appeared
+    protected long estimateN; // estimated total number of data items there will be,
+    // if not -1, sufficient sp
