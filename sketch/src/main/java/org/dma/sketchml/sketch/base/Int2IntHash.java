51734bcc43f31c91d7be39b5bba195ced209a779
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-12-04 09:52:14 +0000

    experimenting

--- sketch/src/main/java/org/dma/sketchml/sketch/base/Int2IntHash.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/base/Int2IntHash.java
@@ -2,4 +2,11 @@ package org.dma.sketchml.sketch.base;
 
 import java.io.Serializable;
 
-p
+public abstract class Int2IntHash implements Serializable {
+    protected int size;
+
+    public Int2IntHash(int size) {
+        this.size = size;
+    }
+
+    public abstract int 
