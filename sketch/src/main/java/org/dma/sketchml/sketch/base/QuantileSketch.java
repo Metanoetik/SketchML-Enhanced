5a88739f6733767b901c33d6595be48f7d207173
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-12-04 12:57:47 +0000

    Some commenting

--- sketch/src/main/java/org/dma/sketchml/sketch/base/QuantileSketch.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/base/QuantileSketch.java
@@ -6,4 +6,10 @@ import java.io.Serializable;
 public abstract class QuantileSketch implements Serializable {
     protected long n; // total number of data items appeared
     protected long estimateN; // estimated total number of data items there will be,
-    // if not -1, sufficient sp
+    // if not -1, sufficient space will be allocated at once
+
+    protected double minValue;
+    protected double maxValue;
+
+    public QuantileSketch(long estimateN) {
+        this.estimateN = estima
