ecd2488c6a470cc1124fe49c4d921dfe1a362e86
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-12-07 10:06:37 +0000

    travis init

--- sketch/src/main/java/org/dma/sketchml/sketch/base/QuantileSketch.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/base/QuantileSketch.java
@@ -45,4 +45,9 @@ public abstract class QuantileSketch implements Serializable {
 
     public double getMinValue() {
         return minValue;
-   
+    }
+
+    public double getMaxValue() {
+        return maxValue;
+    }
+}
---
+++ sketch/src/main/java/org/dma/sketchml/sketch/base/Quantizer.java
@@ -0,0 +1,4 @@
+package org.dma.sketchml.sketch.base;
+
+import org.dma.sketchml.sketch.common.Constants;
+import org.
