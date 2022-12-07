9945d79ea3b834176e382f0a8acfdb7228f55029
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2022-12-07 10:12:30 +0000

    fix eslint errors

--- sketch/src/main/java/org/dma/sketchml/sketch/base/Quantizer.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/base/Quantizer.java
@@ -1,4 +1,8 @@
 package org.dma.sketchml.sketch.base;
 
 import org.dma.sketchml.sketch.common.Constants;
-import org.
+import org.dma.sketchml.sketch.quantization.QuantileQuantizer;
+import org.dma.sketchml.sketch.quantization.UniformQuantizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
