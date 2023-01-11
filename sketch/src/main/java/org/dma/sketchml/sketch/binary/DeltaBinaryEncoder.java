889f60886d1f2749d3c2dfe0d67fad50469fb32a
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-01-11 16:16:53 +0000

    Add version command

--- sketch/src/main/java/org/dma/sketchml/sketch/binary/DeltaBinaryEncoder.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/binary/DeltaBinaryEncoder.java
@@ -2,3 +2,10 @@ package org.dma.sketchml.sketch.binary;
 
 import org.dma.sketchml.sketch.base.BinaryEncoder;
 import org.dma.sketchml.sketch.base.SketchMLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import j
