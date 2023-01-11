cb588b3af339deeb9cc6b51bfeee16a66a6c549b
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-01-11 11:03:08 +0000

    Delete MANIFEST.MF

--- sketch/src/main/java/org/dma/sketchml/sketch/base/SketchMLException.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/base/SketchMLException.java
@@ -1,3 +1,10 @@
 package org.dma.sketchml.sketch.base;
 
-public class SketchMLException extends RuntimeEx
+public class SketchMLException extends RuntimeException {
+    public SketchMLException(String message) {
+        super(message);
+    }
+
+    public SketchMLException(Throwable cause) {
+        super(cause);
+    }
