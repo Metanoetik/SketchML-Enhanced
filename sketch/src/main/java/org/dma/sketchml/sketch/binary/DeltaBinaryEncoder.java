18b69591ce0be99426bef85add5ea0b1ac890ee3
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-02-01 18:39:45 +0000

    Update example app

--- sketch/src/main/java/org/dma/sketchml/sketch/binary/DeltaBinaryEncoder.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/binary/DeltaBinaryEncoder.java
@@ -100,3 +100,5 @@ public class DeltaBinaryEncoder implements BinaryEncoder {
             delta[i] = ois.readLong();
         }
         deltaBits = BitSet.valueOf(delta);
+    }
+}
---
+++ sketch/src/main/java/org/dma/sketchml/sketch/binary/HuffmanEncoder.java
@@ -0,0 +1,5 @@
+package org.dma.sketchml.sketch.binary;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectRBTreeMap;
+impor
