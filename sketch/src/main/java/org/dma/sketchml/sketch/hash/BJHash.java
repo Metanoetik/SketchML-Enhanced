33290c6515bf83969dd152eaf4fe7a59e1459276
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-03-12 16:43:04 +0000

    Code review markups.

--- sketch/src/main/java/org/dma/sketchml/sketch/hash/BJHash.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/hash/BJHash.java
@@ -1,1 +1,7 @@
-package org.dma.sketchml
+package org.dma.sketchml.sketch.hash;
+
+import org.dma.sketchml.sketch.base.Int2IntHash;
+
+public class BJHash extends Int2IntHash {
+    public BJHash(int size) {
+        super(size);
