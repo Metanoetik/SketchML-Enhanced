c73d0b087fd8e4d4776378a42e6d0252e53b802c
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-03-12 23:00:50 +0000

    updated url

--- sketch/src/main/java/org/dma/sketchml/sketch/hash/BJHash.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/hash/BJHash.java
@@ -5,3 +5,9 @@ import org.dma.sketchml.sketch.base.Int2IntHash;
 public class BJHash extends Int2IntHash {
     public BJHash(int size) {
         super(size);
+    }
+
+    public int hash(int key) {
+        int code = key;
+        code = (code + 0x7ed55d16) + (code << 12);
+        code = (code ^ 0xc761c23c) 
