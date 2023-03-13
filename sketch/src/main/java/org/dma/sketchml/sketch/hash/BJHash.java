0fa19639914bcfa3a9cc86b55d44e2c9cb1344ba
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-03-13 11:57:35 +0000

    Simplifying code

--- sketch/src/main/java/org/dma/sketchml/sketch/hash/BJHash.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/hash/BJHash.java
@@ -10,4 +10,7 @@ public class BJHash extends Int2IntHash {
     public int hash(int key) {
         int code = key;
         code = (code + 0x7ed55d16) + (code << 12);
-        code = (code ^ 0xc761c23c) 
+        code = (code ^ 0xc761c23c) ^ (code >> 19);
+        code = (code + 0x165667b1) + (code << 5);
+        code = (code + 0xd3a2646c) ^ (code << 9);
+        code = (code + 0xfd7046c5) + (co
