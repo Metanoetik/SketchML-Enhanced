7a1310ffdb5cd3a9dec74ca127f9ed48d5207953
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-03-16 09:17:19 +0000

    readme 2

--- sketch/src/main/java/org/dma/sketchml/sketch/hash/BKDRHash.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/hash/BKDRHash.java
@@ -2,4 +2,9 @@ package org.dma.sketchml.sketch.hash;
 
 import org.dma.sketchml.sketch.base.Int2IntHash;
 
-public class 
+public class BKDRHash extends Int2IntHash {
+    private int seed;
+
+    public BKDRHash(int size, int seed) {
+        super(size);
+        this.seed = seed
