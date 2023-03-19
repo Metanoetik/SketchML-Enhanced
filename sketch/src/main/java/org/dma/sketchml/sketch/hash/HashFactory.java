a8f3c899a8c5f214ad2d93644ae64400d081043a
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-03-19 06:34:02 +0000

    Google

--- sketch/src/main/java/org/dma/sketchml/sketch/hash/HashFactory.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/hash/HashFactory.java
@@ -1,4 +1,10 @@
 package org.dma.sketchml.sketch.hash;
 
 import org.dma.sketchml.sketch.base.Int2IntHash;
-import o
+import org.dma.sketchml.sketch.base.SketchMLException;
+import org.dma.sketchml.sketch.util.Maths;
+
+import java.util.*;
+
+public class HashFactory {
+    private static final I
