6e32a4309e0ec7100f1bed229abcef08f1605a24
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-03-20 08:11:33 +0000

    Adding Travis CI support

--- sketch/src/main/java/org/dma/sketchml/sketch/hash/HashFactory.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/hash/HashFactory.java
@@ -7,4 +7,7 @@ import org.dma.sketchml.sketch.util.Maths;
 import java.util.*;
 
 public class HashFactory {
-    private static final I
+    private static final Int2IntHash[] int2intHashes =
+            new Int2IntHash[]{new BJHash(0), new Mix64Hash(0),
+            new TWHash(0), new BKDRHash(0, 31), new BKDRHash(0, 131),
+            new
