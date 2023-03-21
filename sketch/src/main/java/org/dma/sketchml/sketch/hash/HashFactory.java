9b7b4aabbc46606ab2420566f3e793a5c5c18338
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-03-21 12:47:15 +0000

    Promote RDO repository to latest consistent URL

--- sketch/src/main/java/org/dma/sketchml/sketch/hash/HashFactory.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/hash/HashFactory.java
@@ -10,4 +10,7 @@ public class HashFactory {
     private static final Int2IntHash[] int2intHashes =
             new Int2IntHash[]{new BJHash(0), new Mix64Hash(0),
             new TWHash(0), new BKDRHash(0, 31), new BKDRHash(0, 131),
-            new
+            new BKDRHash(0, 267), new BKDRHash(0, 1313), new BKDRHash(0, 13131)};
+    private static final Random random = new Random();
+
+    public static Int2IntHash getRandom
