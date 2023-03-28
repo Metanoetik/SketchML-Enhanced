c0b59f4304bf6f9db4df4bbbaa50759c70a05397
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-03-28 09:19:20 +0000

    Improving documentation organization

--- sketch/src/main/java/org/dma/sketchml/sketch/hash/Mix64Hash.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/hash/Mix64Hash.java
@@ -1,1 +1,10 @@
-package org.dma.sketchml.sketch.
+package org.dma.sketchml.sketch.hash;
+
+import org.dma.sketchml.sketch.base.Int2IntHash;
+
+public class Mix64Hash extends Int2IntHash {
+    public Mix64Hash(int size) {
+        super(size);
+    }
+
+    pu
