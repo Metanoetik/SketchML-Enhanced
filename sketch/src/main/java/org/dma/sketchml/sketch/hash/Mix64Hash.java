d5424eeabc22905745fa80bde94ed3455dcb1ac5
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-03-30 18:28:01 +0000

    flake

--- sketch/src/main/java/org/dma/sketchml/sketch/hash/Mix64Hash.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/hash/Mix64Hash.java
@@ -7,4 +7,8 @@ public class Mix64Hash extends Int2IntHash {
         super(size);
     }
 
-    pu
+    public int hash(int key) {
+        int code = key;
+        code = (~code) + (code << 21); // code = (code << 21) - code - 1;
+        code = code ^ (code >> 24);
+    
