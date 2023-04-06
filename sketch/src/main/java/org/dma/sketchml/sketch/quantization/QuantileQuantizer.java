4e9e0a721da8d544b7abba64895c259c2b4ad00d
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-04-06 08:32:00 +0000

    Catch exceptions

--- sketch/src/main/java/org/dma/sketchml/sketch/quantization/QuantileQuantizer.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/quantization/QuantileQuantizer.java
@@ -2,4 +2,10 @@ package org.dma.sketchml.sketch.quantization;
 
 import org.dma.sketchml.sketch.base.Quantizer;
 import org.dma.sketchml.sketch.common.Constants;
-import org.dma.sketchml.sketch.sketch.quantile.HeapQuantileSketc
+import org.dma.sketchml.sketch.sketch.quantile.HeapQuantileSketch;
+import org.dma.sketchml.sketch.util.Maths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executi
