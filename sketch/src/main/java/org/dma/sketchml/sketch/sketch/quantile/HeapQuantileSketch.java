77ec054c58bbe6a4d453fbc4472c38ce2ebe3b0e
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-05-04 21:16:23 +0000

    Updated to Swift 3

--- sketch/src/main/java/org/dma/sketchml/sketch/sketch/quantile/HeapQuantileSketch.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/sketch/quantile/HeapQuantileSketch.java
@@ -1,3 +1,11 @@
 package org.dma.sketchml.sketch.sketch.quantile;
 
-import org.dma.sketchml.sket
+import org.dma.sketchml.sketch.base.QuantileSketch;
+
+import java.util.Arrays;
+
+/**
+ * Implementation of quantile sketch on the Java heap
+ * bashed on `DataSketches` of Yahoo!
+ */
+public c
