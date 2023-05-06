f1c4e378b0a6db5dde5eaf66280458e6025b96e4
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2023-05-06 13:49:29 +0000

    Sloader update on _data/WPF.json

--- sketch/src/main/java/org/dma/sketchml/sketch/sketch/quantile/HeapQuantileSketch.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/sketch/quantile/HeapQuantileSketch.java
@@ -10,4 +10,8 @@ import java.util.Arrays;
  */
 public class HeapQuantileSketch extends QuantileSketch {
     private int k; // parameter that controls space usage
-    public static final int DEF
+    public static final int DEFAULT_K = 128;
+
+    /**
+     * This single array contains the base buffer plus all levels some of which may not be used.
+     * A level is of size K and is either full and sorted, or not
