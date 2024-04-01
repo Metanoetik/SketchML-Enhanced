7f822272df820e35de2cf1846b1ab8faf0bd8db1
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2024-04-01 07:43:39 +0000

    modify Makefile

--- sketch/src/main/java/org/dma/sketchml/sketch/sketch/quantile/QSketchUtils.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/sketch/quantile/QSketchUtils.java
@@ -70,3 +70,74 @@ public class QSketchUtils {
     }
 
     protected static void levelwisePropagation(long bitPattern, int k,
+                                               int beginLevel, int endLevel,
+                                               final double[] buf, int bufBeginPos,
+                                               final double[] levelsArr) {
+        for (int level = beginLevel; level < endLevel; level++) {
+            if ((bitPattern & (1L << level)) == 0)
+                throw new QuantileSketchException("Encounter empty level: " + level);
+            QSketchUtils.mergeArrays(levelsArr, k * (level + 2),
+                    levelsArr, k * (endLevel + 2), buf, bufBeginPos, k);
+            QSketchUtils.compactBuffer(buf, bufBeginPos, levelsArr, k * (endLevel + 2), k);
+        }
+    }
+
+    protected static void blockyMergeSort(final double[] keys, final long[] values,
+                                          int length, int blkSize) {
+        if (blkSize <= 0 || length <= blkSize) return;
+        int numBlks = (length + (blkSize - 1)) / blkSize;
+        final double[] tmpKeys = Arrays.copyOf(keys, length);
+        final long[] tmpValues = Arrays.copyOf(values, length);
+        recursiveBlockyMergeSort(tmpKeys, tmpValues, keys, values, 0, numBlks, blkSize, length);
+    }
+
+    protected static void recursiveBlockyMergeSort(final double[] kSrc, final long[] vSrc,
+                                                   final double[] kDst, final long[] vDst,
+                                                   int blkStart, int blkLen, int blkSize, int arrLimit) {
+        if (blkLen == 1) return;
+        int blkLen1 = blkLen >> 1;
+        int blkLen2 = blkLen - blkLen1;
+        int blkStart1 = blkStart;
+        int blkStart2 = blkStart + blkLen1;
+
+        recursiveBlockyMergeSort(kDst, vDst, kSrc, vSrc, blkStart1, blkLen1, blkSize, arrLimit);
+        recursiveBlockyMergeSort(kDst, vDst, kSrc, vSrc, blkStart2, blkLen2, blkSize, arrLimit);
+
+        int arrStart1 = blkStart1 * blkSize;
+        int arrStart2 = blkStart2 * blkSize;
+        int arrLen1 = blkLen1 * blkSize;
+        int arrLen2 = blkLen2 * blkSize;
+        if (arrStart2 + arrLen2 > arrLimit)
+            arrLen2 = arrLimit - arrStart2;
+
+        blockyMerge(kSrc, vSrc, arrStart1, arrLen1, arrStart2, arrLen2, kDst, vDst, arrStart1);
+    }
+
+    protected static void blockyMerge(final double []kSrc, final long []vSrc,
+                                      int arrStart1, int arrLen1,
+                                      int arrStart2, int arrLen2,
+                                      final double []kDst, final long []vDst, int arrStart3){
+        int arrEnd1 = arrStart1 + arrLen1;
+        int arrEnd2 = arrStart2 + arrLen2;
+        int i1 = arrStart1, i2 = arrStart2, i3 = arrStart3;
+        while (i1 < arrEnd1 && i2 < arrEnd2) {
+            if (kSrc[i1] <= kSrc[i2]){
+                kDst[i3] = kSrc[i1];
+                vDst[i3] = vSrc[i1];
+                ++i1; ++i3;
+            } else {
+                kDst[i3] = kSrc[i2];
+                vDst[i3] = vSrc[i2];
+                ++i2; ++i3;
+            }
+        }
+
+        if (i1 < arrEnd1) {
+            System.arraycopy(kSrc, i1, kDst, i3, arrEnd1 - i1);
+            System.arraycopy(vSrc, i1, vDst, i3, arrEnd1 - i1);
+        } else {
+            System.arraycopy(kSrc, i2, kDst, i3, arrEnd2 - i2);
+            System.arraycopy(vSrc, i2, vDst, i3, arrEnd2 - i2);
+        }
+    }
+}
