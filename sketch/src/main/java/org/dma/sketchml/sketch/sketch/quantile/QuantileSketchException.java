69a30be8f8395cf5b20f8d90d1d39fd5bce29ada
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2024-04-01 08:48:17 +0000

    new line added

--- sketch/src/main/java/org/dma/sketchml/sketch/sketch/quantile/QuantileSketchException.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/sketch/quantile/QuantileSketchException.java
@@ -1,3 +1,10 @@
 package org.dma.sketchml.sketch.sketch.quantile;
 
-import org.dma.sketchml.sketch.base.S
+import org.dma.sketchml.sketch.base.SketchMLException;
+
+public class QuantileSketchException extends SketchMLException {
+    public QuantileSketchException(String message) {
+        super(message);
+    }
+
+  
