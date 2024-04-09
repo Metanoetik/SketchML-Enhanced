f934e773f0f9e1f5064e53ab030559c38142bacc
Metanoetik <150140205+Metanoetik@users.noreply.github.com>
2024-04-09 22:57:04 +0000

    initial .gitignore

--- sketch/src/main/java/org/dma/sketchml/sketch/util/Maths.java
+++ sketch/src/main/java/org/dma/sketchml/sketch/util/Maths.java
@@ -5,3 +5,8 @@ import org.dma.sketchml.sketch.base.SketchMLException;
 import java.util.Random;
 
 public class Maths {
+    public static boolean isPowerOf2(int k) {
+        for (int i = 1; i < 65536; i <<= 1) {
+            if (k == i)
+                return true;
+     
